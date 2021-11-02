f3bacde7e5e85f4b5984161903f9b804ccfb6c7a
dhannushAY <81463007+dhannushAY@users.noreply.github.com>
2021-11-02 23:41:03 +0530

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+
+            <img 
+                src="https://assets.stickpng.com/thumbs/580b585b2edbce24c47b2a27.png"
+                alt="Troll"
+            /> 
+                
+            
+        </header>
+    )
+}
+
+export default Header
